fc0df136764a512fe808c472fadcbc8ec17d5f27
mchen127 <mchen127.p@gmail.com>
2025-08-08 16:37:56 +0800

    fix: server

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 // server.js
+// This file's only job is to start the server.
+
 const app = require("./app"); // Import the configured app
 const PORT = 3000;
 
-// Start the server
-app.listen(PORT, () => {
+// Start the server and export the instance
+const server = app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+module.exports = server;
